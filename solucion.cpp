f151ecf1e92d21c8ade8a7f5a91b3a00211be818
TmsRC <t.rubio28@uniandes.edu.co>
2019-05-07 12:25:31 +0000

    Aun nada

--- solucion.cpp
+++ solucion.cpp
@@ -16,16 +16,39 @@ void resolver() {
     outfile.open("datos.dat");
     
     double L = 1;
-    double deltaX = 0.01;
-    double deltaT= 0.01;
+    double dx = 0.01;
+    double dt= 0.01;
+    double t = 0;
     double tmax = 6;
     double c = 1/2;
+    double c1 = dx/dt;
+    double r = c*c/c1/c1;
     
     
-    double malla[100][2];
+    double malla[100][3];
     
     for(int i=0; i<100; i++) {
-        malla[i][0] = sin(M_PI*i/100)
+        malla[i][0] = sin(M_PI*i/100);
+        malla[i][1] = 0;
+    }
+    
+    while(t<tmax){
+        
+        
+        malla[0][1] = 0;
+        malla[99][2] = 0;
+        for(int i=1; i<99; i++) {
+            malla[i][1] = malla[i][0] + 0.5*r*(malla[i+1][0]+malla[i-1][0]+2*malla[i][0]);
+            malla[i][2] = 2*malla[i][1] - malla[i][0] + c1*(malla[i+1][1]+malla[i-1][1]+2*malla[i][1]);
+            malla[i][1] = 0;
+        }
+        for(int i=0; i<100; i++) {
+            malla[i][0] = malla[i][1];
+            malla[i][1] = malla[i][2]
+        }
+        
+        t+=dt;
+        
     }
     
     
